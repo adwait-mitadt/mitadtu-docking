62f65b8b9886534da0635e0c37713ad4d8405937
kshitijchitre01 <kshitijchitre1@gmail.com>
2025-10-08 10:49:42 +0530

    Changed MSE and MAE to RMSE

--- resnet_model.py
+++ resnet_model.py
@@ -1,3 +1,4 @@
+import keras.backend as K
 
 import keras
 from keras.applications import ResNet50
@@ -5,6 +6,8 @@ from keras.layers import GlobalAveragePooling2D, Dense, Dropout, Input
 from keras.models import Model
 
 def build_resnet_regression():
+	def rmse(y_true, y_pred):
+		return K.sqrt(K.mean(K.square(y_pred - y_true)))
 	input_shape = (224, 224, 3)
 	base_model = ResNet50(weights="imagenet", include_top=False, input_shape=input_shape)
 	base_model.trainable = False  # Freeze all layers
@@ -17,7 +20,7 @@ def build_resnet_regression():
 
 	model = Model(inputs=base_model.input, outputs=outputs)
 	optimizer = keras.optimizers.Adam(learning_rate=1e-4)
-	model.compile(optimizer=optimizer, loss="mse", metrics=["mae"])
+	model.compile(optimizer=optimizer, loss=rmse, metrics=[rmse])
 	return model
 
 if __name__ == "__main__":
